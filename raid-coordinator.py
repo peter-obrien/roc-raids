9da7dcede0f344363b47b2f04a0341dcaa92649c
Peter O'Brien <pjo2412@gmail.com>
2017-08-02 18:58:45 -0400

    Removed restriction from working in General.

--- raid-coordinator.py
+++ raid-coordinator.py
@@ -35,118 +35,119 @@ async def on_ready():
 @client.event
 async def on_message(message):
 
-    if message.channel.name != 'general':
-        if message.content.startswith('!go'):
-            message = await client.get_message(message.channel, '341294312749006849')
+    if message.channel.name == 'gymhuntr' and message.server.message.content.startswith('!go'):
+        message = await client.get_message(message.channel, '341294312749006849')
 
-        if message.author.name == 'GymHuntrBot':
-            if len(message.embeds) > 0:
-                gmapUrl = googleDirectionsUrlBase + message.embeds[0]['url'].split('#')[1]
+    if message.author.name == 'GymHuntrBot':
+        if len(message.embeds) > 0:
+            gmapUrl = googleDirectionsUrlBase + message.embeds[0]['url'].split('#')[1]
 
-                descTokens = message.embeds[0]['description'].split('\n')
-                gymName = descTokens[0]
-                pokemon = descTokens[1]
+            descTokens = message.embeds[0]['description'].split('\n')
+            gymName = descTokens[0]
+            pokemon = descTokens[1]
 
-                timeTokens = descTokens[3].split(' ')
-                secondsToEnd = int(timeTokens[6]) + (60 * int(timeTokens[4])) + (60 * 60 * int(timeTokens[2]))
-                endTime = message.timestamp + timedelta(seconds=secondsToEnd)
-                easternEndTime = endTime.replace(tzinfo=utcTz).astimezone(easternTz)
+            timeTokens = descTokens[3].split(' ')
+            secondsToEnd = int(timeTokens[6]) + (60 * int(timeTokens[4])) + (60 * 60 * int(timeTokens[2]))
+            endTime = message.timestamp + timedelta(seconds=secondsToEnd)
+            easternEndTime = endTime.replace(tzinfo=utcTz).astimezone(easternTz)
 
-                desc = gymName + '\n' + '*Ends: ' + easternEndTime.strftime(timeFmt) + '*'
+            desc = gymName + '\n' + '*Ends: ' + easternEndTime.strftime(timeFmt) + '*'
 
-                raid = raids.create_raid(pokemon, gymName, easternEndTime)
+            raid = raids.create_raid(pokemon, gymName, easternEndTime)
 
-                if raid.id is None:
-                    raid.id = raids.generate_raid_id()
-                    raids.store_raid(raid)
+            if raid.id is None:
+                raid.id = raids.generate_raid_id()
+                raids.store_raid(raid)
 
-                    result = discord.Embed(title=pokemon + ': Raid #' + str(raid.id), url=gmapUrl, description=desc, colour=embedColor)
+                result = discord.Embed(title=pokemon + ': Raid #' + str(raid.id), url=gmapUrl, description=desc, colour=embedColor)
 
-                    thumbnailContent = message.embeds[0]['thumbnail']
-                    result.set_thumbnail(url=thumbnailContent['url'])
-                    result.thumbnail.height=thumbnailContent['height']
-                    result.thumbnail.width=thumbnailContent['width']
-                    result.thumbnail.proxy_url=thumbnailContent['proxy_url']
+                thumbnailContent = message.embeds[0]['thumbnail']
+                result.set_thumbnail(url=thumbnailContent['url'])
+                result.thumbnail.height=thumbnailContent['height']
+                result.thumbnail.width=thumbnailContent['width']
+                result.thumbnail.proxy_url=thumbnailContent['proxy_url']
 
-                    raid.embed = result
+                raid.embed = result
 
-                raidMessage = await client.send_message(message.channel, embed=raid.embed)
-                raid.add_message(raidMessage)
-                if message.id != '341294312749006849':
+            raidMessage = await client.send_message(message.channel, embed=raid.embed)
+            raid.add_message(raidMessage)
+            if message.id != '341294312749006849':
+                await client.delete_message(message)
+    else:
+        if message.content.startswith('!who '):
+            raidId = message.content[5:]
+            try:
+                raid = raids.get_raid(raidId)
+                msg = raid.get_raiders()
+                await client.send_message(message.author, msg)
+            except InputError as err:
+                await client.send_message(message.author, err.message)
+            finally:
+                if not message.channel.is_private:
                     await client.delete_message(message)
-        else:
-            if message.content.startswith('!who '):
-                raidId = message.content[5:]
-                try:
-                    raid = raids.get_raid(raidId)
-                    msg = raid.get_raiders()
-                    await client.send_message(message.author, msg)
-                except InputError as err:
-                    await client.send_message(message.author, err.message)
-                finally:
-                    if not message.channel.is_private:
-                        await client.delete_message(message)
 
-            elif message.content.startswith('!join '):
-                commandDetails = message.content[6:].split(' ')
-                raidId = commandDetails[0]
-                party_size = '1'
-                start_time = None
-                if len(commandDetails) > 1:
-                    party_size = commandDetails[1]
-                if len(commandDetails) > 2:
-                    start_time = ' '.join(str(x) for x in commandDetails[2:])
-                try:
-                    raid = raids.get_raid(raidId)
-                    displayMsg = raid.add_raider(message.author.display_name, party_size, start_time)
+        elif message.content.startswith('!join '):
+            commandDetails = message.content[6:].split(' ')
+            raidId = commandDetails[0]
+            party_size = '1'
+            start_time = None
+            if len(commandDetails) > 1:
+                party_size = commandDetails[1]
+            if len(commandDetails) > 2:
+                start_time = ' '.join(str(x) for x in commandDetails[2:])
+            try:
+                raid = raids.get_raid(raidId)
+                displayMsg = raid.add_raider(message.author.display_name, party_size, start_time)
+                for msg in raid.messages:
+                    await client.edit_message(msg, embed=raid.embed)
+                await client.send_message(message.channel, displayMsg)
+                if not message.channel.is_private:
+                    await client.delete_message(message)
+            except InputError as err:
+                await client.send_message(message.author, err.message)
+                if not message.channel.is_private:
+                    await client.delete_message(message)
+
+        elif message.content.startswith('!leave '):
+            raidId = message.content[7:]
+            try:
+                raid = raids.get_raid(raidId)
+                displayMsg = raid.remove_raider(message.author.display_name)
+                if displayMsg is not None:
                     for msg in raid.messages:
                         await client.edit_message(msg, embed=raid.embed)
                     await client.send_message(message.channel, displayMsg)
-                    await client.delete_message(message)
-                except InputError as err:
-                    await client.send_message(message.author, err.message)
-                    if not message.channel.is_private:
-                        await client.delete_message(message)
-
-            elif message.content.startswith('!leave '):
-                raidId = message.content[7:]
-                try:
-                    raid = raids.get_raid(raidId)
-                    displayMsg = raid.remove_raider(message.author.display_name)
-                    if displayMsg is not None:
-                        for msg in raid.messages:
-                            await client.edit_message(msg, embed=raid.embed)
-                        await client.send_message(message.channel, displayMsg)
-                    await client.delete_message(message)
-                except InputError as err:
-                    await client.send_message(message.author, err.message)
-                    if not message.channel.is_private:
-                        await client.delete_message(message)
-
-            elif message.content.startswith('!details '):
-                raidId = message.content[9:]
-                try:
-                    raid = raids.get_raid(raidId)
-                    await client.send_message(message.author, embed=raid.embed)
-                except InputError as err:
-                    await client.send_message(message.author, err.message)
-                finally:
-                    if not message.channel.is_private:
-                        await client.delete_message(message)
-
-            elif message.content.startswith('!raid '): # alias for !details
-                raidId = message.content[6:]
-                try:
-                    raid = raids.get_raid(raidId)
-                    await client.send_message(message.author, embed=raid.embed)
-                except InputError as err:
-                    await client.send_message(message.author, err.message)
-                finally:
-                    if not message.channel.is_private:
-                        await client.delete_message(message)
-            elif message.content.startswith('!'):
-                await client.send_message(message.author, embed=helpMessage)
                 if not message.channel.is_private:
                     await client.delete_message(message)
+            except InputError as err:
+                await client.send_message(message.author, err.message)
+                if not message.channel.is_private:
+                    await client.delete_message(message)
+
+        elif message.content.startswith('!details '):
+            raidId = message.content[9:]
+            try:
+                raid = raids.get_raid(raidId)
+                await client.send_message(message.author, embed=raid.embed)
+            except InputError as err:
+                await client.send_message(message.author, err.message)
+            finally:
+                if not message.channel.is_private:
+                    await client.delete_message(message)
+
+        elif message.content.startswith('!raid '): # alias for !details
+            raidId = message.content[6:]
+            try:
+                raid = raids.get_raid(raidId)
+                await client.send_message(message.author, embed=raid.embed)
+            except InputError as err:
+                await client.send_message(message.author, err.message)
+            finally:
+                if not message.channel.is_private:
+                    await client.delete_message(message)
+        elif message.content.startswith('!'):
+            await client.send_message(message.author, embed=helpMessage)
+            if not message.channel.is_private:
+                await client.delete_message(message)
 
 client.run(sys.argv[1])
