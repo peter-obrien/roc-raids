5933b8368ec91d38d83e64ab3637c9a5c7f1c4d7
Peter O'Brien <pjo2412@gmail.com>
2017-08-01 23:06:25 -0400

    More rewording of the usage command.

--- raid-coordinator.py
+++ raid-coordinator.py
@@ -22,8 +22,8 @@ embedColor = 0x408fd0
 helpMessage=discord.Embed(title="Commands", description="Here are the commands that the roc-raids bot recognizes.", color=0xf0040b)
 helpMessage.add_field(name="!join [raid-id] (party-size) (start-time)", value="Use this command to signal to others that you wish to attend the raid. The message with the specified raid-id will be updated to reflect your party's size. Can be used again to overwrite your previous party for the raid.", inline=False)
 helpMessage.add_field(name="!leave [raid-id]", value="Can't make the raid you intended to join? Use this to take your party off the list.", inline=False)
-helpMessage.add_field(name="!raid [raid-id]", value="Receive a PM from the bot with the raid summary. Can also use !details [raid-id]", inline=False)
-helpMessage.add_field(name="!who [raid-id]", value="Receive a PM from the bot with the details of those that are attending the raid via the !join command.", inline=False)
+helpMessage.add_field(name="!raid [raid-id]", value="Receive a PM from the bot with the raid summary. This contains the gym name, pokemon, raid end time and Google Maps location. Can also use !details [raid-id]", inline=False)
+helpMessage.add_field(name="!who [raid-id]", value="Receive a PM from the bot with the details of who is attending the raid.", inline=False)
 
 @client.event
 async def on_ready():
