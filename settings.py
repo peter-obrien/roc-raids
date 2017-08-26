a39583f096fce89ccc69f67731e03649f241cf30
Peter O'Brien <pjo2412@gmail.com>
2017-08-26 10:22:20 -0400

    Changed time zone.

--- settings.py
+++ settings.py
@@ -47,7 +47,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
