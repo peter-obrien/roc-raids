a089b2377fcd2e0fe5db9eeef5e5e33cd29efc34
Peter O'Brien <pjo2412@gmail.com>
2017-08-27 00:16:36 -0400

    Model for RaidZone

--- orm/models.py
+++ orm/models.py
@@ -1,5 +1,6 @@
 from django.contrib.postgres.fields import JSONField
 from django.db import models
+from math import sin, cos, sqrt, atan2, radians
 
 
 class Raid(models.Model):
@@ -60,3 +61,58 @@ class RaidParticipant(models.Model):
 
 class BotOnlyChannel(models.Model):
     channel = models.CharField(max_length=64)
+
+
+def filter_default():
+    return {'pokemon': [], 'raid_levels': []}
+
+
+class RaidZone(models.Model):
+    destination = models.CharField(max_length=64)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    radius = models.DecimalField(max_digits=3, decimal_places=1, default=5.0)
+    active = models.BooleanField(default=True)
+    filters = JSONField(default=filter_default)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Since the destination field is the ID of the discord channel/user, this attribute will hold the full object.
+        self.discord_destination = None
+
+    def filter(self, raid):
+        if self.active:
+            if self._isInRaidZone(raid):
+                return self._filter_pokemon(raid)
+        else:
+            return False
+
+    def _isInRaidZone(self, raid):
+        earth_radius = 6373.0
+
+        center_lat = radians(self.latitude)
+        center_lon = radians(self.longitude)
+        gym_lat = radians(raid.latitude)
+        gym_lon = radians(raid.longitude)
+
+        lon_diff = gym_lon - center_lon
+        lat_diff = gym_lat - center_lat
+
+        a = sin(lat_diff / 2) ** 2 + cos(center_lat) * cos(gym_lat) * sin(lon_diff / 2) ** 2
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+        distance = earth_radius * c
+
+        return distance <= self.radius
+
+    def _filter_pokemon(self, raid):
+        if len(self.filters['pokemon']) > 0:
+            return int(raid.pokemon_number) in self.filters['pokemon']
+        else:
+            return True
+
+    def _filter_raid_level(self, raid):
+        if len(self.filters['raid_levels']) > 0:
+            return int(raid.raid_level) in self.filters['raid_levels']
+        else:
+            return False
