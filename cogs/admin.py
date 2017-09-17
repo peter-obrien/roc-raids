c73d241732fe5b029b21f48a03caee3167fffa60
Peter O'Brien <pjo2412@gmail.com>
2017-09-16 20:36:41 -0400

    Hid the commands from Help and fixed parameters.

--- cogs/admin.py
+++ cogs/admin.py
@@ -10,47 +10,44 @@ class Admin:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.command(hidden=True)
     @commands.guild_only()
-    async def clear(self, ctx, *msg_count_to_delete: str):
+    async def clear(self, ctx, msg_count_to_delete: int = 5):
+        """Deletes groups of messages. Cannot delete messages older than 14 days."""
         if ctx.author == ctx.guild.owner:
-            if len(msg_count_to_delete) != 1 or not msg_count_to_delete[0].isdigit() or int(
-                    msg_count_to_delete[0]) > 100 or not isinstance(ctx.channel,
-                                                                    discord.TextChannel):
-                await ctx.send(content='Please only provide a number of messages to delete less than 100.')
-            else:
-                try:
-                    message_to_delete = []
-                    async for message in ctx.message.channel.history(limit=int(msg_count_to_delete[0])):
-                        message_to_delete.append(message)
-                    await ctx.channel.delete_messages(message_to_delete)
-                except discord.HTTPException:
-                    # Assume that messages older than 14 days were found and delete one at a time.
-                    async for message in ctx.message.channel.history(limit=int(msg_count_to_delete[0])):
-                        await message.delete()
+            try:
+                message_to_delete = []
+                async for message in ctx.message.channel.history(limit=msg_count_to_delete):
+                    message_to_delete.append(message)
+                await ctx.channel.delete_messages(message_to_delete)
+            except discord.HTTPException:
+                # Assume that messages older than 14 days were found and delete one at a time.
+                async for message in ctx.message.channel.history(limit=msg_count_to_delete):
+                    await message.delete()
         else:
             raise commands.CommandInvokeError('User cannot run this command.')
 
-    @commands.command()
-    async def logout(self, ctx, *msg_count_to_delete: str):
-        if ctx.bot.is_owner(ctx.author):
-            print('Logout command invoked. Shutting down.')
-            await ctx.message.delete()
-            await ctx.bot.logout()
-        else:
-            raise commands.CommandInvokeError('User cannot run this command.')
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def logout(self, ctx):
+        """Logs the bot out of Discord."""
+        print('Logout command invoked. Shutting down.')
+        await ctx.message.delete()
+        await ctx.bot.logout()
 
-    @commands.command()
+    @commands.command(hidden=True)
     @commands.guild_only()
-    async def botonly(self, ctx, *value: str):
-        toggle_value = value[0]
-        if toggle_value == 'on':
+    @commands.has_permissions(manage_channels=True)
+    async def botonly(self, ctx, value: str):
+        """Allows user to toggle if a channel only accepts bot commands.
+        """
+        if value.lower() == 'on':
             if ctx.channel not in ctx.bot.bot_only_channels:
                 boc = BotOnlyChannel(channel=ctx.channel.id)
                 boc.save()
                 ctx.bot.bot_only_channels.append(ctx.channel)
             await ctx.send('Bot only commands enabled.')
-        elif toggle_value == 'off':
+        elif value.lower() == 'off':
             if ctx.channel in ctx.bot.bot_only_channels:
                 boc = BotOnlyChannel.objects.get(channel=ctx.channel.id)
                 boc.delete()
