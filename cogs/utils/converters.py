5b5e9b5623df9cfa69ae359b78bb8ebfb663e117
Peter O'Brien <pjo2412@gmail.com>
2018-03-24 14:33:12 -0400

    Renamed UserRaidEndTime to UserRaidEndTimeAndDate. Created new UserRaidEndTime converter.

--- cogs/utils/converters.py
+++ cogs/utils/converters.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from discord.ext import commands
+from django.utils import timezone
 
 
 class ChannelOrMember(commands.Converter):
@@ -11,9 +12,20 @@ class ChannelOrMember(commands.Converter):
             return await commands.MemberConverter().convert(ctx, argument)
 
 
-class UserRaidEndTime(commands.Converter):
+class UserRaidEndTimeAndDate(commands.Converter):
     async def convert(self, ctx, argument):
         try:
             return datetime.strptime(argument, '%m/%d/%y %H:%M')
         except ValueError:
             raise commands.BadArgument(f'Unable to parse end time `{argument}` expected "mm/dd/yy 24H:MM"')
+
+
+class UserRaidEndTime(commands.Converter):
+    async def convert(self, ctx, argument):
+        try:
+            current_date = timezone.localdate(timezone.now())
+            user_time = datetime.strptime(argument, '%H:%M')
+            return datetime(year=current_date.year, month=current_date.month, day=current_date.day, hour=user_time.hour,
+                            minute=user_time.minute)
+        except ValueError:
+            raise commands.BadArgument(f'Unable to parse end time `{argument}` expected "24H:MM"')
